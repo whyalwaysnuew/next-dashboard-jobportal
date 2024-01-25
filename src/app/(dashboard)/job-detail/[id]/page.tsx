431080f4919f2e36c4f676a6471a569743b5090e
whyalwaysnuew <coworasavanila@gmail.com>
2024-01-25 11:59:23 +0700

    Prod: Final

--- src/app/(dashboard)/job-detail/[id]/page.tsx
+++ src/app/(dashboard)/job-detail/[id]/page.tsx
@@ -1,58 +1,61 @@
-import { ArrowLeftIcon } from 'lucide-react';
-import Link from 'next/link';
-import React, { FC } from 'react'
+import {ArrowLeftIcon} from "lucide-react";
+import Link from "next/link";
+import React, {FC} from "react";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
-import Applicants from '@/components/organisms/Applicants';
-import JobDetails from '@/components/organisms/JobDetail';
-import prisma from '../../../../../lib/prisma';
-
+import Applicants from "@/components/organisms/Applicants";
+import JobDetails from "@/components/organisms/JobDetail";
+import prisma from "../../../../../lib/prisma";
 
 type paramsType = {
   id: string;
-}
+};
 
 interface JobDetailPageProps {
   params: paramsType;
 }
 
+export const revalidate = 0;
+
 async function getDetailJob(id: string) {
   const job = await prisma.job.findFirst({
     where: {
-      id: id
+      id: id,
     },
     include: {
       Applicant: {
         include: {
-          user: true
-        }
+          user: true,
+        },
       },
-      CategoryJob: true
-    }
-  })
+      CategoryJob: true,
+    },
+  });
 
   return job;
 }
 
-const JobDetailPage: FC<JobDetailPageProps> = async ({ params }) => {
-
-  const job = await getDetailJob(params.id)  
+const JobDetailPage: FC<JobDetailPageProps> = async ({params}) => {
+  const job = await getDetailJob(params.id);
 
   return (
     <div>
       <div className="inline-flex items-center gap-5 mb-5">
         <div className="">
-          <Link className='hover:text-primary' href="/job-listings">
+          <Link className="hover:text-primary" href="/job-listings">
             <ArrowLeftIcon className="w-9 h-9" />
           </Link>
         </div>
         <div className="">
           <div className="text-2xl font-semibold mb-1">{job?.roles}</div>
-          <div>{job?.CategoryJob?.name} . {job?.jobType} . {job?.applicants}/{job?.needs} Hired</div>
+          <div>
+            {job?.CategoryJob?.name} . {job?.jobType} . {job?.applicants}/
+            {job?.needs} Hired
+          </div>
         </div>
       </div>
 
       <Tabs defaultValue="applicants">
-        <TabsList className='mb-8'>
+        <TabsList className="mb-8">
           <TabsTrigger value="applicants">Applicants</TabsTrigger>
           <TabsTrigger value="jobdetails">Job Details</TabsTrigger>
         </TabsList>
@@ -65,6 +68,6 @@ const JobDetailPage: FC<JobDetailPageProps> = async ({ params }) => {
       </Tabs>
     </div>
   );
-}
+};
 
 export default JobDetailPage;
